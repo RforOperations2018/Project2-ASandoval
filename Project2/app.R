d01699e0362b82c5af83c544f55426e052bcb6b8
AlexSandoval <a.sandoval0014>
2018-10-18 16:09:50 -0400

    Added downloadable data

--- Project2/app.R
+++ Project2/app.R
@@ -1,70 +1,3 @@
-# # Upload packages
-# library(rgdal)
-# require(leaflet)
-# require(leaflet.extras)
-# require(dplyr)
-# require(readxl)
-# require(stringr)
-# 
-# 
-# 
-# # 2018 Philadelphia Marathon Route
-# # This data was created from http://geojson.io/#map=2/20.0/0.0.
-# # Point and line data was traced from the 2018 Philadelphia marathon website, https://philadelphiamarathon.phila.gov/#/
-# # Neighborhoods were downloaded from https://github.com/azavea/geo-data
-# 
-# # Upload polylines, polypoints, and polygons
-# route <- readOGR("marathon.geojson")
-# plot(route)
-# markers <- readOGR("finalmarkers.geojson")
-# plot(markers)
-# hoods <- readOGR("Neighborhoods_Philadelphia.geojson")
-# plot(hoods)
-# 
-# # Add an if else statement to change colors of the markers
-# getColor <- function(markers) {
-#   sapply(markers$marker.symbol, function(marker.symbol) {
-#     if(marker.symbol %in% "S") {
-#       "green"
-#     } else if(marker.symbol %in% "F") {
-#       "red"
-#     } else {
-#       "orange"
-#     } })
-# }
-# 
-# # Add icons
-# icons <- awesomeIcons(
-#   text = ~as.character(marker.symbol),
-#   markerColor = getColor(markers)
-# )
-# 
-# # Choose Icon Legend
-# html_legend <- "Philadelphia Marathon 2018 Route <br/> <br/> <img src='https://png.icons8.com/material-outlined/50/000000/marker.png'>Mile Markers "
-# 
-# # Plot map
-# leaflet() %>%
-#   # Add Philly 2018 Marathon Route
-#   addPolylines(data = route, color = "#000000", fillOpacity = 1, weight = 10) %>%
-#   # Add Philly Neighborhoods
-#   addPolygons(data = hoods, color = "#1ab2ff", label = ~mapname, fillOpacity = 0.00, weight = 3, highlightOptions = highlightOptions(color = "red", bringToFront = TRUE)) %>%
-#   # Add Philly 2018 Mile Markers
-#   addAwesomeMarkers(data = markers, icon = icons, label = ~as.character(marker.symbol)) %>%
-#   # Add Basemaps
-#   addProviderTiles(providers$OpenMapSurfer.Grayscale, options = providerTileOptions(noWrap = TRUE)) %>%
-#   addTiles(options = providerTileOptions(noWrap = TRUE), group = "Default") %>%
-#   addProviderTiles("Esri.WorldImagery", options = providerTileOptions(noWrap = TRUE), group = "Imagery") %>%
-#   addProviderTiles("Esri.WorldTerrain", options = providerTileOptions(noWrap = TRUE), group = "Terrain") %>%
-#   # Add Layers control
-#   addLayersControl(
-#     baseGroups = c("Default", "Imagery", "Terrain"),
-#     options = layersControlOptions(collapsed = FALSE)
-#   ) %>%
-#   # Set View
-#   setView( lat = 39.980379,  lng= -75.165490, zoom = 12.1) %>%
-#   # Add Legend
-#   addControl(html_legend, position = "bottomleft")
-
 library(shiny)
 library(reshape2)
 library(dplyr)
@@ -77,7 +10,6 @@ library(grid)
 library(scales)
 
 sale.upload <- read.csv ("sales_2.csv")
-property.load <- read.csv ("projectdata_7.csv")
 
 sale.load <- sale.upload %>%
   mutate(
@@ -105,7 +37,7 @@ sidebar <- dashboardSidebar(
   sidebarMenu(
     id = "tabs",
     menuItem("Plot", icon = icon("bar-chart"), tabName = "plot"),
-    menuItem("Data Table", icon = icon("table"), tabName = "table"),
+    menuItem("Download Data", icon = icon("download"), tabName = "table"),
 
     # Category Select
     selectInput("categorySelect",
@@ -136,12 +68,9 @@ sidebar <- dashboardSidebar(
                   "Is the Property Ready for Auction?",
                   choices = c("Yes", "No"),
                   multiple = FALSE,
-                  selected = "Yes"),
+                  selected = "Yes")
 
-   # Download Data Buttion
-   tabItem("table",
-           inputPanel(
-             downloadButton("downloadData","Download Sheriff Sale Data")))
+
   ))
 
 body <- dashboardBody(tabItems(
@@ -155,14 +84,20 @@ body <- dashboardBody(tabItems(
            # names of the plot tabs
            fluidRow(
              tabBox(title = "Plot", width = 12,
-                    tabPanel("Property Change of Value", plotlyOutput("plot_value")),
-                    tabPanel("Sum of taxes owed by Zip Code", plotlyOutput("plot_properties"))))
-          ),
+                    tabPanel("Property Change of Value", plotlyOutput("plot_types")),
+                    tabPanel("Sum of taxes owed by Zip Code", plotlyOutput("plot_taxes"))))),
   
-   # Table name
-   tabItem("table",
-           fluidPage(
-             box(title = "Pittsburgh Sheriff Sales Properties ", DT::dataTableOutput("table"), width = 12)))
+  # Download Data
+  tabItem("table",
+          inputPanel(
+            downloadButton("downloadData","Download Sheriff Sale Data") # add button to download table as csv
+          ),
+          fluidPage(
+            box(title = "Pittsburgh Sheriff Sales Properties", DT::dataTableOutput("table"), width = 12)))
+
+  
+  
+  
  ))
 
  ui <- dashboardPage(header, sidebar, body)
@@ -185,10 +120,13 @@ body <- dashboardBody(tabItems(
     }
     return(property)
    })
+   #Reactive data
+   mInput <- reactive({
+     property <- propInput()
+   })
    
-
-   # Plot 1-  Counts of Properties by Sale Tpes
-   output$plot_value <- renderPlotly({
+   # Plot 1-  Counts of Properties by Sale Types
+   output$plot_types <- renderPlotly({
      property <- propInput()
      ggplot(data = sale.load,
             aes(x = SaleType,
@@ -202,7 +140,7 @@ body <- dashboardBody(tabItems(
    })
    
    # Plot 2- Plot showing taxes owed by zip code
-   output$plot_properties <- renderPlotly({
+   output$plot_taxes <- renderPlotly({
      property <- propInput ()
      ggplot (data = sale.load,
             aes (x = ZIPCode,
@@ -221,26 +159,35 @@ body <- dashboardBody(tabItems(
      subset(propInput(), select = c(DocketNumber, SaleType, AttorneyName, Plaintiff, Defendant, SaleDate, Address, CostsTaxes))
    })
 
-   # Average current market value box
+   # Common Attorney
    output$attorney <- renderInfoBox({
      proper <- propInput()
      name <- names(sort(table(sale.load$AttorneyName), decreasing = TRUE))
      valueBox(subtitle = "Is the most common Attorney", value = name, icon = icon("briefcase"),  color = "green")
    })
    
-   # Average sale price box
+   # Average Taxes Owed box
    output$avgtaxes <- renderValueBox({
      proper <- propInput()
      nums <- prettyNum(round(mean(sale.load$CostsTaxes, na.rm = T), 0))
      valueBox(subtitle = "Average Taxes Owed ", value = nums, icon = icon("usd"), color = "red")
    })
    
-   # Total Taxable Land box
+   # Most in a zipcode
    output$zipcode <- renderValueBox({
      proper <- propInput()
      name <- names(sort(table(sale.load$ZIPCode), decreasing = TRUE))
      valueBox(subtitle = "This Zipcode has the most Sheriff Sales", value = name, icon("home"), color = "blue")
    })
+   
+   # Make data downloadable and set default download name
+   output$downloadData <- downloadHandler(
+     filename = function() {
+       paste("sheriff-sale-data-", Sys.Date(), ".csv", sep="")
+     },
+     content = function(file) {
+       write.csv(propInput(), file)
+     })
  }
  # Run the application
 shinyApp(ui = ui, server = server)
